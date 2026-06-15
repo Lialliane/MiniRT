b1e5e3e886023955f784ddc8f079daae175589db
lujain <lojain01k@gmail.com>
2026-06-15 18:35:18 +0300

    Added essential vector functions

--- main.c
+++ main.c
@@ -9,9 +9,72 @@
 #define WIDTH 512
 #define HEIGHT 512
 
+typedef struct s_vector
+{
+	double x;
+	double y;
+	double z;
+} t_vec;
+
+typedef struct s_ray
+{
+	t_vec origin;
+	t_vec direction;
+} t_ray;
+
 static mlx_image_t* image;
 
-// -----------------------------------------------------------------------------
+t_vec vec_sum(t_vec vec1, t_vec vec2)
+{
+	t_vec sum_vec;
+
+	sum_vec.x = vec1.x + vec2.x;
+	sum_vec.y = vec1.y + vec2.y;
+	sum_vec.z = vec1.z + vec2.z;
+	return (sum_vec);
+}
+
+t_vec vec_sub(t_vec vec1, t_vec vec2)
+{
+	t_vec sub_vec;
+
+	sub_vec.x = vec1.x - vec2.x;
+	sub_vec.y = vec1.y - vec2.y;
+	sub_vec.z = vec1.z - vec2.z;
+	return (sub_vec);
+}
+
+t_vec vec_scale(t_vec vec, int scalar)
+{
+	t_vec mult_vec;
+
+	mult_vec.x = vec.x * scalar;
+	mult_vec.y = vec.y * scalar;
+	mult_vec.z = vec.z * scalar;
+	return (mult_vec);
+}
+
+// returns a unit vector in the same direction as the input vector
+// done to make comparison of vectors direction easier 
+t_vec vec_normalize(t_vec vec)
+{
+	int magnitude;
+	t_vec norm_vec;
+
+	magnitude = ft_pow(vec.x, 2) + ft_pow(vec.y, 2) + ft_pow(vec.z, 2);
+	if (magnitude == 0)
+		return ((t_vec){0, 0, 0});
+	norm_vec.x = vec.x / magnitude;
+	norm_vec.y = vec.y / magnitude;
+	norm_vec.z = vec.z / magnitude;
+	return (norm_vec);
+}
+
+int dot_product(t_vec vec1, t_vec vec2)
+{
+	return (vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z);
+}
+
 
 int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
@@ -34,6 +97,16 @@ int	ft_pow(int nb, int power)
 		result *= ft_pow(nb, power - 1);
 		return (result);
 	}
+}
+
+t_vec vec_init(int x, int y, int z)
+{
+    t_vec vec;
+
+    vec.x = x;
+    vec.y = y;
+    vec.z = z;
+    return (vec);
 }
 
 void ft_draw_sphere(void* param)
